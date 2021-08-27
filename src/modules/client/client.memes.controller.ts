c039dbe9b3323b4c0a5ca7d4f3d1736589a92eec
thearturca <arti.1046@gmail.com>
2021-08-27 15:33:50 +0500

    WIP

--- src/modules/client/client.memes.controller.ts
+++ src/modules/client/client.memes.controller.ts
@@ -1,4 +1,5 @@
 import { ChatUserstate } from "tmi.js-reply-fork";
+import { MemesTypes } from "../../domain/memes/entities/memes.factory-entity";
 import { MemesGetMemeResponseEntity } from "../../domain/memes/entities/memes.get-meme.response-entity";
 import { MemesGetMemeCommand } from "../../domain/memes/ports/in/memes.get-meme.command";
 import { MemesGetMemePort } from "../../domain/memes/ports/in/memes.get-meme.port";
@@ -19,6 +20,15 @@ export class ClientMemesController {
         const getMemeCommand: MemesGetMemeCommand = new MemesGetMemeCommand(simplifiedMessage, username)
         const getMemeResponse: MemesGetMemeResponseEntity = await this._memesGetMemePort.getMeme(getMemeCommand);
         if (!getMemeResponse.status) return new ClientResponseEntity(ClientResponseType.none);
-        return new ClientResponseEntity(ClientResponseType.reply, user, getMemeResponse.memeMessage);
+        switch(getMemeResponse.meme) {
+            case MemesTypes.bigracc:
+            case MemesTypes.gachi:
+            case MemesTypes.pugpls:
+                return new ClientResponseEntity(ClientResponseType.say, user, getMemeResponse.memeMessage);
+            case MemesTypes.toilet:
+                return new ClientResponseEntity(ClientResponseType.reply, user, getMemeResponse.memeMessage);
+            default:
+                return new ClientResponseEntity(ClientResponseType.reply, user, getMemeResponse.memeMessage);
+        }
     }
 }
